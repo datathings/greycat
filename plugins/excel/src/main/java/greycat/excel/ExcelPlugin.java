a9d6bb59d332adddb238c142fd06bad42d5f16e7
Grégory Nain <gregory.nain@gmail.com>
2017-03-06 16:52:12 +0100

    update

--- plugins/excel/src/main/java/greycat/excel/ExcelPlugin.java
+++ plugins/excel/src/main/java/greycat/excel/ExcelPlugin.java
@@ -9,17 +9,35 @@ import greycat.Type;
 import greycat.plugin.ActionFactory;
 import greycat.plugin.Plugin;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by gnain on 27/02/17.
  */
 public class ExcelPlugin implements Plugin {
+
+    private Path _basePath;
+    /**
+     * Excel plugin constructor
+     * @param basePath Base path from which files can be reached
+     */
+    public ExcelPlugin(String basePath) {
+        _basePath = Paths.get(basePath);
+    }
 
     @Override
     public void start(Graph graph) {
         graph.actionRegistry().declaration(ExcelActions.LOAD_XSLX).setParams(Type.STRING).setFactory(new ActionFactory() {
             @Override
             public Action create(Object[] params) {
-                return new ActionLoadXlsx(String.valueOf(params[0]));
+                Path resolved = _basePath.resolve(String.valueOf(params[0]));
+                if(resolved.toFile().exists()) {
+                    return new ActionLoadXlsx(resolved.toString());
+                } else {
+                    return new ActionLoadXlsx(String.valueOf(params[0]));
+                }
+
             }
         });
     }
