ac914f02514bb32e459c3a1e49d5d1f036166d53
Grégory Nain <gregory.nain@gmail.com>
2017-03-06 16:52:00 +0100

    fix

--- plugins/blas/src/test/java/greycatBlasTest/GraphTest.java
+++ plugins/blas/src/test/java/greycatBlasTest/GraphTest.java
@@ -23,29 +23,26 @@ public class GraphTest {
                 .withPlugin(new BlasPlugin())
                 .build();
 
-        g.connect(new Callback<Boolean>() {
-            @Override
-            public void on(Boolean result) {
-                RandomGenerator generator=new RandomGenerator();
-                generator.setSeed(1234);
-                DMatrix vm = VolatileDMatrix.random(200, 1000, generator, 1, 2);
-                DMatrix vm2 = VolatileDMatrix.random(1000, 300, generator, -1, 1);
+        g.connect(result -> {
+            RandomGenerator generator = new RandomGenerator();
+            generator.setSeed(1234);
+            DMatrix vm = VolatileDMatrix.random(200, 1000, generator, 1, 2);
+            DMatrix vm2 = VolatileDMatrix.random(1000, 300, generator, -1, 1);
 
-                long start = System.currentTimeMillis();
-                DMatrix mult = MatrixOps.multiply(vm, vm2);
-                long end = System.currentTimeMillis();
-                System.out.println("Blas Matrix Engine took: " + (end - start) + " ms");
+            long start = System.currentTimeMillis();
+            DMatrix mult = MatrixOps.multiply(vm, vm2);
+            long end = System.currentTimeMillis();
+            System.out.println("Blas Matrix Engine took: " + (end - start) + " ms");
 
-                MatrixOps.setDefaultEngine(new PlainMatrixEngine());
-                start = System.currentTimeMillis();
-                DMatrix mult2 = MatrixOps.multiply(vm, vm2);
-                end = System.currentTimeMillis();
-                System.out.println("Plain Matrix Engine took: " + (end - start) + " ms");
+            MatrixOps.setDefaultEngine(new PlainMatrixEngine());
+            start = System.currentTimeMillis();
+            DMatrix mult2 = MatrixOps.multiply(vm, vm2);
+            end = System.currentTimeMillis();
+            System.out.println("Plain Matrix Engine took: " + (end - start) + " ms");
 
-                double eps = 1e-10;
-                Assert.assertTrue(MatrixOps.compare(mult, mult2) < eps);
+            double eps = 1e-10;
+            Assert.assertTrue(MatrixOps.compare(mult, mult2) < eps);
 
-            }
         });
 
     }
