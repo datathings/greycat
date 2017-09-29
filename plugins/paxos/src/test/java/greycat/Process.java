43a08f91ad3f3b9068aa4b54810a632997c54787
Francois Fouquet <fouquet.f@gmail.com>
2017-09-29 17:22:31 +0200

    fix

--- plugins/paxos/src/test/java/greycat/Process.java
+++ plugins/paxos/src/test/java/greycat/Process.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.*;
 import java.util.List;
 
@@ -30,19 +29,25 @@ public class Process {
     public static void main(String[] args) {
         HashMap<Integer, List<Integer>> fusion = new HashMap<Integer, List<Integer>>();
         readCSV("/Users/duke/Documents/datathings/greycat/plugins/paxos/src/test/java/greycat/FusionResults.csv", fusion);
+
+        HashMap<Integer, List<Integer>> gdlock = new HashMap<Integer, List<Integer>>();
+        readCSV("/Users/duke/Documents/datathings/greycat/plugins/paxos/src/test/java/greycat/gdlock/Result.csv", gdlock);
+
         HashMap<Integer, List<Integer>> raft = new HashMap<Integer, List<Integer>>();
         readCSV("/Users/duke/Documents/datathings/greycat/plugins/paxos/src/test/java/greycat/RaftResults.csv", raft);
 
-        HashMap<Integer, List<Integer>> raft6 = new HashMap<Integer, List<Integer>>();
-        readCSV("/Users/duke/Documents/datathings/greycat/plugins/paxos/src/test/java/greycat/Raft6.csv", raft6);
+        HashMap<Integer, List<Integer>> raft7 = new HashMap<Integer, List<Integer>>();
+        readCSV("/Users/duke/Documents/datathings/greycat/plugins/paxos/src/test/java/greycat/Raft6.csv", raft7);
 
         List<Integer> speedFusion = computeSpeed(fusion);
+        List<Integer> speedDlock = computeSpeed(gdlock);
         List<Integer> speedRaft = computeSpeed(raft);
-        List<Integer> speedRaft6 = computeSpeed(raft6);
+        List<Integer> speedRaft7 = computeSpeed(raft7);
 
         System.out.println("fusionAvg=" + computeAvg(speedFusion));
+        System.out.println("dlockAvg=" + computeAvg(speedDlock));
         System.out.println("raftAvg=" + computeAvg(speedRaft));
-        System.out.println("raft6Avg=" + computeAvg(speedRaft6));
+        System.out.println("raft7Avg=" + computeAvg(speedRaft7));
         /*
         StringBuilder buffer = new StringBuilder();
         print("fusion", speedFusion, buffer);
@@ -55,7 +60,10 @@ public class Process {
         } catch (IOException e) {
             // do something
         }*/
-        SimpleBoxPlot(speedFusion, speedRaft, speedRaft6);
+        //SimpleBoxPlot(speedFusion, speedRaft, speedRaft6);
+
+        SimpleBoxPlot(new String[]{"LockFree_Merge", "CentralServer_Lock","RaftConsensus_2_Peers","RaftConsensus_7_Peers"},speedFusion, speedDlock, speedRaft, speedRaft7);
+
     }
 
     public static void print(String name, List<Integer> elems, StringBuilder buffer) {
@@ -139,13 +147,24 @@ public class Process {
     protected static final Color COLOR1 = new Color(55, 170, 200);
     protected static final Color COLOR2 = new Color(200, 80, 75);
 
-    public static void SimpleBoxPlot(List<Integer> fusion, List<Integer> raft, List<Integer> raft6) {
+    public static void SimpleBoxPlot(String[] names, List<Integer>... datasets) {
 
         // Create example data
-        DataTable data = new DataTable(Integer.class, Integer.class, Integer.class);
+        DataTable data = new DataTable(datasets.length, Integer.class);
 
-        for (int i = 0; i < fusion.size(); i++) {
-            data.add(fusion.get(i), raft.get(i % raft.size()), raft6.get(i % raft6.size()));
+        int maxSize = 0;
+        for (int i = 0; i < datasets.length; i++) {
+            if (maxSize < datasets[i].size()) {
+                maxSize = datasets[i].size();
+            }
+        }
+
+        for (int i = 0; i < maxSize; i++) {
+            List<Integer> raw = new ArrayList<Integer>(datasets.length);
+            for (int j = 0; j < datasets.length; j++) {
+                raw.add(datasets[j].get(i % datasets[j].size()));
+            }
+            data.add(raw);
         }
 
         // Create new box-and-whisker plot
@@ -158,17 +177,33 @@ public class Process {
         plot.setInsets(new Insets2D.Double(20.0, 70.0, 40.0, 20.0));
 
         // Format axes
+
+        Double[] ids = new Double[datasets.length];
+        for (int i = 0; i < datasets.length; i++) {
+            ids[i] = i + 1.0;
+        }
         plot.getAxisRenderer(BoxPlot.AXIS_X).setCustomTicks(
                 DataUtils.map(
-                        new Double[]{1.0, 2.0, 3.0},
-                        new String[]{"Fusion", "Raft", "Raft6"}
+                        ids,
+                        names
                 )
         );
-
         Axis axisY = new Axis();
-        axisY.setRange(0.0, 40000);
+
+        int max = 0;
+        for (int i = 0; i < datasets.length; i++) {
+            List<Integer> dataset = datasets[i];
+            for (int j = 0; j < dataset.size(); j++) {
+                int val = dataset.get(j);
+                if (max < val) {
+                    max = val;
+                }
+            }
+        }
+
+        axisY.setRange(0.0, max);
         AxisRenderer axisRendererY = new LogarithmicRenderer2D();
-        axisRendererY.setTickSpacing(1.5);
+        axisRendererY.setTickSpacing(2.6);
         plot.setAxis(BoxPlot.AXIS_Y, axisY);
         plot.setAxisRenderer(BoxPlot.AXIS_Y, axisRendererY);
 
@@ -187,12 +222,12 @@ public class Process {
         // Add plot to Swing component
         InteractivePanel panel = new InteractivePanel(plot);
         JFrame frame = new JFrame("Hello");
-        panel.setPreferredSize(new Dimension(1024, 768));
-        panel.setSize(new Dimension(1024, 768));
+        panel.setPreferredSize(new Dimension(1200, 400));
+        panel.setSize(new Dimension(1200, 5-400));
         frame.getContentPane().add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(1024, 768));
-        frame.setSize(new Dimension(1024, 768));
+        frame.setPreferredSize(new Dimension(1200, 400));
+        frame.setSize(new Dimension(1200, 400));
         //frame.pack();
         frame.setVisible(true);
     }
