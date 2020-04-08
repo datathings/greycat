a5abf6a18af8f56c4ecc8741ad6653d9a0918912
Matthieu JIMENEZ <matthieu.jimenez@datathings.com>
2020-04-08 18:23:26 +0200

    fix polynomial export

--- plugins/ml/src/main/java/greycat/ml/regression/PolynomialNode.java
+++ plugins/ml/src/main/java/greycat/ml/regression/PolynomialNode.java
@@ -522,7 +522,7 @@ public class PolynomialNode extends BaseMLNode implements RegressionNode {
                         node.get(INTERNAL_NB_PAST_KEY) + "," +
                         node.getWithDefault(INTERNAL_STEP_KEY, 1L);
                 for (int j = 0; j < weight.length; j++) {
-                    String[] number = ("" + (weight[j])).split(".");
+                    String[] number = ("" + (weight[j])).split("\\.");
                     line += "," + number[0];
                     if (number.length == 2) {
                         int min = Math.min(3, number[1].length());
